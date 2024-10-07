e75e6621374873971d0c007e067e6377b1d137b5
B1oko <pablo.eb01@gmail.com>
2024-10-07 18:54:30 +0200

    added check_winner function

--- script.sh
+++ script.sh
@@ -1,1 +1,21 @@
-h
+#!/bin/bash
+
+
+
+
+
+
+function check_winner(){
+    nombre_carperta=$1
+    minutes=$(date +"%M")
+
+    if [ $((minutes % 2)) -eq 0 ]; then
+        echo "Has ganado!!"
+        echo "Has ganado!!" >> ./$nombre_carperta/ganador.txt
+    else
+        echo "Has perdido!!"
+        echo "Has perdido!!" >> ./$nombre_carperta/ganador.txt
+    fi
+}
+
+check_winner $name
